f9b6ab170e5a55a2df7c98bf1885f6346a55d925
Pavithra <pavithrasai9398@gmail.com>
2025-03-21 10:44:27 +0400

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ require('dotenv').config();
 const app = express();
 
 const allowedOrigins = [
-  'https://real-estate-frontend-gray.vercel.app',
+  'https://real-estate-eta-steel.vercel.app',
   'http://localhost:3000', // Add your local development URL
 ];
 
